45d43440effa67eccd8c82987ba4a9eb81e04668
safethecode <iam@sonjim.in>
2024-06-30 21:04:13 +0900

    refactor: 스토리 섹션 리팩토링

---
+++ src/components/StorySection.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="story-section">
+    <img src="@/assets/line.svg" alt="line" class="story-section__line">
+    <p class="story-section__title">
+      하나님 사랑을 주위에 나누며<br/>
+      <span class="story-section__title--highlight">살아가는 가정이 되겠습니다.</span>
+    </p>
+    <div class="story-section__content-container">
+      <img src="@/assets/images/Rectangle.svg" alt="Background" class="story-section__background">
+      <div class="story-section__content">
+        <p class="story-section__text">
+          처음 만났던 날,<br/>
+          서로가 기도하며 꿈꿔온<br/>
+          믿음의 가정에 대해 나누며<br/>
+          약 2년이라는 시간 동안<br/>
+          서로의 햇빛과 나무가 돼주었고
+        </p>
+        <p class="story-section__text">
+          2024년 08월 24일<br/>
+          우리를 만나게 하신 하나님 앞에<br/>
+          그리고 소중한 여러분 앞에 하나가 되는<br/>
+          아름다운 서약을 하려 합니다.
+        </p>
+        <p class="story-section__text">
+          언제나 서로 사랑하며 감사가 넘치는<br/>
+          믿음의 가정 이루도록 함께 오셔서<br/>
+          저희의 시작을 축복해 주시면 감사하겠습니다.
+        </p>
+        <div class="story-section__names">
+          <p class="story-section__name">노수련 · 김유미 의 장남 노승찬</p>
+          <p class="story-section__name">신준철 · 이선경 의 장녀 신지원</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'StorySection',
+}
+</script>
+
+<style scoped>
+@import "@/components/scss/StorySection.scss";
+</style>
