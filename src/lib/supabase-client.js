98357ea80ecc53b778bd6fc8ad64b68e676d8b24
safethecode <iam@sonjim.in>
2024-07-21 11:30:24 +0900

    refactor: env

--- src/lib/supabase-client.js
+++ src/lib/supabase-client.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-import process from "process";
 
 export const supabase = createClient(
   "https://fykzbdtxkyjhaaaisgun.supabase.co",
-  process.env.VUE_APP_SUPABASE_KEY
+  import.meta.env.VITE_SUPABASE_KEY
 );
